daabd9dbf45f0fa256c29b9422b4a0df4514ac28
Ben21312312321312 <113138632+Ben21312312321312@users.noreply.github.com>
2025-02-07 13:19:05 -0500

    Add files via upload

---
+++ Main.ps1
@@ -0,0 +1,9 @@
+﻿# Dot-source the Apache-Logs.ps1 file
+. .\Apache-Logs.ps1
+
+# Call the function with the specified inputs
+$result = Get-LogIPs -Page "page1.html" -HttpCode "404" -BrowserName "Chrome"
+
+# Display the result
+Write-Output "IP Addresses matching criteria (Page: page1.html, HTTP Code: 404, Browser: Chrome):"
+$result
